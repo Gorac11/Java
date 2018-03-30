f89eec474eb8cf17869c4ad1214ab2e06f163cd3
Gorac11 <37901312+Gorac11@users.noreply.github.com>
2018-03-31 01:55:15 +0300

    Update IntegerToRoman.java

--- Mega/src/IntegerToRoman.java
+++ Mega/src/IntegerToRoman.java
@@ -1,29 +1,33 @@
 import java.util.Scanner;
 public class IntegerToRoman {
-	    public static String ToRoman(int n){
+	    public static String ToRoman(int num){
 	        String roman="";
 	        int repeat;
 	        int magnitude[]={1000,900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 	        String symbol[]={"M","CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 	        
-	        repeat=n/1;
-	        for(int x=0; n>0; x++){
-	            repeat=n/magnitude[x];
-	            //System.out.println("Test: "+ n/1+" "+magnitude[x]+" "+n/magnitude[x]);	//Shows how it works
+	        repeat=num/1;
+	        for(int x=0; num>0; x++){
+	            repeat=num/magnitude[x];
+	            //System.out.println("Test: "+ num/1+" \t"+magnitude[x]+" \tmatches: "+num/magnitude[x]+" \tCycle: "+x);//Shows how it works
 	            for(int i=1; i<=repeat; i++){
 	                roman=roman + symbol[x];
-	                //System.out.println(roman);		//Shows how the string forms after every cycle
-	            }
-	            n=n%magnitude[x];
-	        }
+	                //System.out.println(roman+"   left: "+(repeat-i));		//Shows how the string forms after every match
+	            }	//inner for loop ends
+	            num=num%magnitude[x];
+	        }	//outer for loop ends
 	        return roman;
 	    }
 	 
 	    public static void main(String args[]){
-	    	Scanner sc=new Scanner(System.in);
-	    	int num=sc.nextInt();
-	        System.out.println(num+": "+ToRoman(num));
-	       
+	    	int num;
+	    	do{ 
+	    		System.out.println("Insert positive integer upto 3999:");
+	    		Scanner sc=new Scanner(System.in);
+	    		num=sc.nextInt();
+	    	
+	    	}while(num>=4000);
+	    	System.out.println(num+": "+ToRoman(num));
+	    	
 	    }
 	}
-
